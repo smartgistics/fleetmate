8da549f482145049549faf99dc736a2708e0b3f6
Shane Riley <shane@shaneriley.com>
2024-12-11 15:36:47 -0500

    feat(orders): Create first two steps of new order form wizard

---
+++ components/FormWizard/FormWizard.tsx
@@ -0,0 +1,157 @@
+import { CheckCircle as CheckIcon } from '@mui/icons-material'
+import { Button as MuiButton, Typography } from '@mui/material'
+import cs from 'clsx'
+import {
+  MutableRefObject,
+  ReactNode,
+  useImperativeHandle,
+  useState,
+} from 'react'
+
+import { FormFooter } from '@/components'
+import { Button } from '@/components/Button'
+
+import styles from './FormWizard.module.sass'
+
+export interface FormWizardStep {
+  component: ReactNode
+  name: ReactNode | string
+  submitText?: string
+  hideIcon?: boolean
+  validateComplete: () => boolean
+}
+
+export interface FormWizardProps {
+  classes?: {
+    activeStep?: string
+    completedStep?: string
+    step?: string
+    stepIcon?: string
+    stepItem?: string
+    stepNav?: string
+  }
+  disableSubmit?: boolean
+  disableStepSelection?: boolean
+  footerControls?: ReactNode
+  onCancel: () => void
+  onSubmit: () => boolean
+  resetSteps?: MutableRefObject<() => void>
+  hideCancelButton?: boolean
+  hideFooterOnLastStep?: boolean
+  stepHeader?: ReactNode
+  steps: FormWizardStep[]
+}
+
+export const FormWizard = ({
+  classes,
+  disableStepSelection = false,
+  disableSubmit = false,
+  footerControls,
+  onCancel,
+  onSubmit,
+  resetSteps,
+  hideCancelButton = false,
+  hideFooterOnLastStep = false,
+  stepHeader,
+  steps,
+}: FormWizardProps) => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const activeStep = steps[activeIndex] || steps[0]
+  const [completedSteps, setCompletedSteps] = useState([])
+  const isLastStep = activeIndex === steps.length - 1
+
+  const handleReset = () => {
+    setActiveIndex(0)
+    setCompletedSteps([])
+  }
+
+  useImperativeHandle(resetSteps, () => handleReset, [])
+
+  const onStepSubmit = async (e) => {
+    e.preventDefault()
+    if (!activeStep.validateComplete()) {
+      return
+    }
+    if (!completedSteps.includes(activeStep.name)) {
+      setCompletedSteps((prevSteps) => [...prevSteps, activeStep.name])
+    }
+
+    const nextStepIndex = activeIndex + 1
+
+    if (!steps[nextStepIndex]) {
+      await onSubmit()
+      return
+    }
+
+    setActiveIndex(nextStepIndex)
+  }
+
+  const chooseStep = (step: number) => {
+    if (disableStepSelection) {
+      return
+    }
+    setActiveIndex(step)
+  }
+
+  return (
+    <form
+      action=""
+      className={styles.root}
+      method="post"
+      onSubmit={onStepSubmit}
+    >
+      <div className={styles.main}>
+        <ol className={cs(styles.stepNav, classes?.stepNav)}>
+          {stepHeader && <li>{stepHeader}</li>}
+          {steps.map((step, i) => (
+            <li
+              className={cs(
+                {
+                  [styles.activeStep]: activeStep?.name === step.name,
+                  [styles.completedStep]: completedSteps.includes(step.name),
+                },
+                classes?.stepItem,
+                { [classes?.activeStep]: activeStep?.name === step.name }
+              )}
+              key={i}
+            >
+              <MuiButton onClick={() => chooseStep(i)}>
+                {!step.hideIcon && (
+                  <CheckIcon className={cs(styles.icon, classes?.stepIcon)} />
+                )}
+                <Typography component="div">{step.name}</Typography>
+              </MuiButton>
+            </li>
+          ))}
+        </ol>
+        <section className={cs(styles.steps, classes?.step)}>
+          {activeStep.component}
+        </section>
+      </div>
+
+      {(!isLastStep || (isLastStep && !hideFooterOnLastStep)) && (
+        <FormFooter>
+          <Button
+            color="success"
+            disabled={disableSubmit}
+            onClick={onStepSubmit}
+            type="submit"
+          >
+            {activeStep.submitText || 'Next'}
+          </Button>
+          {!hideCancelButton && onCancel && (
+            <Button
+              color="neutral"
+              onClick={onCancel}
+              size="large"
+              variant="text"
+            >
+              Cancel
+            </Button>
+          )}
+          {footerControls}
+        </FormFooter>
+      )}
+    </form>
+  )
+}
--- components/orders/NewOrderModal.tsx
+++ components/orders/NewOrderModal.tsx
@@ -1,76 +1,312 @@
 'use client'
 
-import React, { useState } from 'react'
+import cs from 'clsx'
+import React, { Fragment, useState } from 'react'
+import { v4 } from 'uuid'
+import { Close as CloseIcon } from '@mui/icons-material'
+import {
+  FormGroup,
+  FormControlLabel,
+  IconButton,
+  Switch,
+  Typography,
+} from '@mui/material'
+
 import { FormData } from '@/types/truckmate'
 import { OrderModalTabs } from './modal/OrderModalTabs'
 import { OrderModalFooter } from './modal/OrderModalFooter'
 import { CustomerTab } from './tabs/CustomerTab'
 import { OrderDetailsTab } from './tabs/OrderDetailsTab'
 import { FinancialsTab } from './tabs/FinancialsTab'
+import { Button } from '@/components/Button'
+import { FormWizard } from '@/components/FormWizard'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+import styles from './NewOrderModal.module.sass'
 
 interface NewOrderModalProps {
   isOpen: boolean
   onClose: () => void
 }
 
-export function NewOrderModal({ isOpen, onClose }: NewOrderModalProps) {
-  const [activeTab, setActiveTab] = useState<string>('customer')
-  const [formData, setFormData] = useState<FormData>({
-    // Customer Tab
-    customer: '',
-    customerReference: '',
-    customerContact: '',
-    contactPhone: '',
-    contactEmail: '',
-    billingAddress: '',
+const CustomersStep = ({ fields, setFields }) => {
+  const localFields = [
+    [
+      {
+        title: 'Account Information',
+        fields: {
+          customerName: 'Company Name',
+          parentCompany: 'Parent Company',
+          customerId: 'Customer ID',
+          status: 'Status',
+          revenue: 'Revenue',
+          creditStatus: 'Credit Status',
+        },
+      },
+    ],
+    [
+      {
+        title: 'Contact Information',
+        fields: {
+          email: 'Email',
+          phone: 'Phone',
+        },
+      },
+      {
+        title: 'Order Owners',
+        fields: {
+          accountManager: 'Account Manager',
+          orderPlanner: 'Order Planner',
+        },
+      },
+    ],
+  ]
 
-    // Order Details Tab
-    contractType: '',
+  const handleChange = ({ target: { name, value } }) =>
+    setFields({ ...fields, [name]: value })
+
+  return (
+    <article>
+      {localFields.map((column, i) => (
+        <div className={styles.column} key={`column_${i}`}>
+          {column.map((section, j) => (
+            <Fragment key={`section_${j}`}>
+              <Typography variant="h5">{section.title}</Typography>
+              {Object.entries(section.fields).map(([name, label], k) => (
+                <Fragment key={`field_${k}`}>
+                  <Label htmlFor={name}>{label}</Label>
+                  <Input
+                    id={name}
+                    name={name}
+                    onChange={handleChange}
+                    value={fields[name]}
+                  />
+                </Fragment>
+              ))}
+            </Fragment>
+          ))}
+        </div>
+      ))}
+    </article>
+  )
+}
+
+const initialCommodity = {
+  code: '',
+  weight: 0,
+  pieces: 0,
+  pallets: 0,
+  cube: 0,
+  volume: 0,
+}
+
+const OrderDetails = ({ fields, setFields }) => {
+  const [commodity, setCommodity] = useState(initialCommodity)
+  const onAdd = () => {
+    setFields({
+      ...fields,
+      commodities: [...fields.commodities, { ...commodity, id: v4() }],
+    })
+    setCommodity(initialCommodity)
+  }
+  const onRemove = (id) => {
+    setFields({
+      ...fields,
+      commodities: fields.commodities.filter((c) => c.id !== id),
+    })
+  }
+
+  const handleChange = ({ target: { name, value } }) =>
+    setFields({ ...fields, [name]: value })
+
+  const handleCommodityChange = ({ target: { name, value } }) =>
+    setCommodity({ ...commodity, [name]: value })
+
+  return (
+    <article>
+      <Input
+        name="orderType"
+        onChange={handleChange}
+        placeholder="Order Type"
+        value={fields.orderType}
+      />
+      <Input
+        name="serviceType"
+        onChange={handleChange}
+        placeholder="Service Type"
+        value={fields.serviceType}
+      />
+      <Input
+        name="equipmentType"
+        onChange={handleChange}
+        placeholder="Equipment Type"
+        value={fields.equipmentType}
+      />
+      <Input
+        name="serviceLevel"
+        onChange={handleChange}
+        placeholder="Service Level"
+        value={fields.serviceLevel}
+      />
+
+      <div className={styles.tempRange}>
+        <FormGroup>
+          <FormControlLabel
+            control={<Switch checked={fields.temperatureControlled} />}
+            label="Temperature Controlled?"
+            onChange={({ target: { checked } }) =>
+              setFields({ ...fields, temperatureControlled: checked })
+            }
+          />
+        </FormGroup>
+        <Label htmlFor="temperatureMin">Range:</Label>
+        <Input
+          name="temperatureMin"
+          onChange={handleChange}
+          placeholder="Min"
+          type="number"
+          value={fields.temperatureMin}
+        />
+        <span>°F -</span>
+        <Input
+          name="temperatureMax"
+          onChange={handleChange}
+          placeholder="Max"
+          type="number"
+          value={fields.temperatureMax}
+        />
+        <span>°F</span>
+      </div>
+
+      <div className={styles.commodities}>
+        <Typography component="p" variant="body2">
+          Add the commodities attached to this order
+        </Typography>
+
+        <div className={styles.commodityFields}>
+          <div>
+            <Label htmlFor="weight">Weight</Label>
+            <Input
+              name="weight"
+              onChange={handleCommodityChange}
+              type="number"
+              value={commodity.weight}
+            />
+            <Typography variant="caption">lbs</Typography>
+          </div>
+          <div>
+            <Label htmlFor="pieces">Pieces</Label>
+            <Input
+              name="pieces"
+              onChange={handleCommodityChange}
+              type="number"
+              value={commodity.pieces}
+            />
+          </div>
+          <div>
+            <Label htmlFor="pallets">Pallets</Label>
+            <Input
+              name="pallets"
+              onChange={handleCommodityChange}
+              type="number"
+              value={commodity.pallets}
+            />
+          </div>
+          <div>
+            <Label htmlFor="cube">Cube</Label>
+            <Input
+              name="cube"
+              onChange={handleCommodityChange}
+              type="number"
+              value={commodity.cube}
+            />
+          </div>
+          <div>
+            <Label htmlFor="volume">Volume</Label>
+            <Input
+              name="volume"
+              onChange={handleCommodityChange}
+              type="number"
+              value={commodity.volume}
+            />
+          </div>
+        </div>
+        <div className={styles.flex}>
+          <Input
+            name="code"
+            onChange={handleCommodityChange}
+            placeholder="Commodity Code and Description"
+            value={commodity.code}
+          />
+          <Button color="success" onClick={onAdd}>
+            Add
+          </Button>
+        </div>
+
+        <ul>
+          {fields.commodities.map((c) => (
+            <li key={c.id}>
+              <Typography variant="h5">{c.code}</Typography>
+              <Typography color="neutral" variant="body2">
+                {c.weight} lbs, {c.pieces} pieces, {c.pallets} pallets, {c.cube}{' '}
+                cube, {c.volume} volume
+              </Typography>
+              <IconButton onClick={() => onRemove(c.id)}>
+                <CloseIcon />
+              </IconButton>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </article>
+  )
+}
+
+export const NewOrderModal = (props) => {
+  const { isOpen, onClose } = props
+  const [fields, setFields] = useState({
+    customerName: '',
+    parentCompany: '',
+    customerId: '',
+    status: '',
+    revenue: '',
+    creditStatus: '',
+    email: '',
+    phone: '',
+    accountManager: '',
+    orderPlanner: '',
+    orderType: '',
+    serviceType: '',
     equipmentType: '',
     serviceLevel: '',
     temperatureControlled: false,
-    tempMin: '',
-    tempMax: '',
-    commodityCode: '',
+    temperatureMin: undefined,
+    temperatureMax: undefined,
     commodities: [],
-
-    // Financials Tab
-    customerCharges: [],
-    carrierCharges: [],
-    miscCharges: [],
-
-    // Additional Fields
-    notes: '',
-    accountManager: '',
-    orderPlanner: '',
-    status: 'New',
-    parentAccount: '',
-    customerId: '',
-    creditStatus: '',
   })
 
-  if (!isOpen) return null
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log('Form Data:', formData)
-    onClose()
+  const handleSubmit = () => {
+    return true
   }
 
-  const handleNext = () => {
-    const tabs = ['customer', 'orderDetails', 'financials']
-    const currentIndex = tabs.indexOf(activeTab)
-    if (currentIndex < tabs.length - 1) {
-      setActiveTab(tabs[currentIndex + 1])
-    }
-  }
-
-  const isLastTab = () => {
-    return activeTab === 'financials'
-  }
+  const formSteps = [
+    {
+      name: 'Customer',
+      component: <CustomersStep fields={fields} setFields={setFields} />,
+      submitText: 'Next: Order Details',
+      validateComplete: () => true,
+    },
+    {
+      name: 'Order Details',
+      component: <OrderDetails fields={fields} setFields={setFields} />,
+      submitText: 'Next: Pickup',
+      validateComplete: () => true,
+    },
+  ]
 
   return (
-    <div className="fixed inset-0 z-50 overflow-y-auto">
+    <div className={cs('fixed inset-0 z-50 overflow-y-auto', styles.root)}>
       <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
@@ -79,64 +315,23 @@ export function NewOrderModal({ isOpen, onClose }: NewOrderModalProps) {
         <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-5xl sm:w-full">
           <div className="bg-white">
             {/* Header */}
-            <div className="flex justify-between items-center px-6 py-4 border-b">
-              <h3 className="text-2xl font-semibold text-gray-900">
+            <header className="flex justify-between items-center px-6 py-4 border-b">
+              <Typography className="text-2xl font-semibold text-gray-900">
                 Create New Order
-              </h3>
+              </Typography>
               <button
                 className="text-gray-400 hover:text-gray-500"
                 onClick={onClose}
               >
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M6 18L18 6M6 6l12 12"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                  />
-                </svg>
+                <CloseIcon />
               </button>
-            </div>
-
-            {/* Tabs */}
-            <div className="border-b border-gray-200">
-              <OrderModalTabs
-                activeTab={activeTab}
-                setActiveTab={setActiveTab}
-              />
-            </div>
-
-            {/* Content */}
-            <form onSubmit={handleSubmit}>
-              <div className="px-6 py-4">
-                {activeTab === 'customer' && (
-                  <CustomerTab formData={formData} setFormData={setFormData} />
-                )}
-                {activeTab === 'orderDetails' && (
-                  <OrderDetailsTab
-                    formData={formData}
-                    setFormData={setFormData}
-                  />
-                )}
-                {activeTab === 'financials' && (
-                  <FinancialsTab
-                    formData={formData}
-                    setFormData={setFormData}
-                  />
-                )}
-              </div>
-              <OrderModalFooter
-                isLastTab={isLastTab()}
-                onClose={onClose}
-                onNext={handleNext}
-              />
-            </form>
+            </header>
           </div>
+          <FormWizard
+            onCancel={onClose}
+            onSubmit={handleSubmit}
+            steps={formSteps}
+          />
         </div>
       </div>
     </div>
